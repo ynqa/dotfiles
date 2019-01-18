436cab740baa5f63b1aeb17c6e3b26867f2b6699
ynqa <un.pensiero.vano@gmail.com>
2019-01-18 22:18:33 +0900

    Yourself

--- install.sh
+++ install.sh
@@ -2,33 +2,11 @@
 
 SCRIPT_ROOT=$(cd $(dirname $0); pwd)
 
-init_brew() {
-    if test ! $(which brew); then
-        echo "Install homebrew..."
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-
-    echo "Install brew file..."
-    brew install rcmdnk/file/brew-file
-
-    echo "Install java8..."
-    brew cask install caskroom/versions/java8
-
-    echo "Install brew packages from Brewfile..."
-    brew file install -f Brewfile
-}
-
 init_zsh() {
     echo "Set zsh to \$SHELL..."
     if [ ${SHELL} != "/bin/zsh" ]; then
         chsh -s /bin/zsh
     fi
-}
-
-move_bash() {
-    latest=$(ls -Art /usr/local/Cellar/bash/ | tail -n 1)
-    sudo mv /bin/bash /bin/bash_old
-    sudo ln -s /usr/local/Cellar/bash/${latest}/bin/bash /bin/bash
 }
 
 init_anyenv() {
@@ -48,62 +26,18 @@ link_dots() {
     done
 }
 
-link_brew() {
-    echo "Link Brewfile under \${HOME}/.config/brewfile/Brewfile"
-    mkdir ${HOME}/.config/brewfile
-    ln -snfv ${SCRIPT_ROOT}/Brewfile ${HOME}/.config/brewfile/Brewfile
-}
-
-install_vscode_package() {
-    echo "Install vscode packages"
-    code --install-extension ms-python.python
-    code --install-extension ms-vscode.cpptools
-    code --install-extension robertohuertasm.vscode-icons
-    code --install-extension ms-vscode.go
-    code --install-extension PeterJausovec.vscode-docker
-    code --install-extension mauve.terraform
-    code --install-extension DevonDCarew.bazel-code
-}
-
 link_vscode_setting() {
     echo "Link vscode setting.json"
     mkdir -p ${HOME}/Library/Application\ Support/Code/User/
     ln -snfv ${SCRIPT_ROOT}/settings.json ${HOME}/Library/Application\ Support/Code/User/settings.json
 }
 
-make_workspace() {
-    cp -r workspace/ ${HOME}/workspace
-    cd ${HOME}/workspace
-    direnv allow
-}
-
-# go_pkg for vscode.
-install_go_pkg() {
-    GOPATH=${HOME}/workspace go get -u -v github.com/nsf/gocode
-    GOPATH=${HOME}/workspace go get -u -v github.com/rogpeppe/godef
-    GOPATH=${HOME}/workspace go get -u -v github.com/zmb3/gogetdoc
-    GOPATH=${HOME}/workspace go get -u -v github.com/golang/lint/golint
-    GOPATH=${HOME}/workspace go get -u -v github.com/lukehoban/go-outline
-    GOPATH=${HOME}/workspace go get -u -v sourcegraph.com/sqs/goreturns
-    GOPATH=${HOME}/workspace go get -u -v golang.org/x/tools/cmd/gorename
-    GOPATH=${HOME}/workspace go get -u -v github.com/tpng/gopkgs
-    GOPATH=${HOME}/workspace go get -u -v github.com/newhook/go-symbols
-    GOPATH=${HOME}/workspace go get -u -v golang.org/x/tools/cmd/guru
-    GOPATH=${HOME}/workspace go get -u -v github.com/cweill/gotests/...
-}
-
 add_gitignore() {
     git config --global core.excludesfile ${HOME}/.gitignore_global
 }
 
-init_brew
 init_zsh
-move_bash
 init_anyenv
 link_dots
-link_brew
-install_vscode_package
 link_vscode_setting
-make_workspace
-install_go_pkg
 add_gitignore
